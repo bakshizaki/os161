8dcf01eaba74a52c3687f33376b4962618f53386
Zaki Bakshi <bakshizaki@gmail.com>
2017-01-20 05:08:50 -0500

    rwt1 written, needs to be changed though

--- kern/test/rwtest.c
+++ kern/test/rwtest.c
@@ -16,12 +16,105 @@
  * Use these stubs to test your reader-writer locks.
  */
 
+//TODO:write tests for following conditions
+//1) make sure writers are not interrupted by other writers / data is not corrupted
+//2) make sure writers are not starved / served as soon as possible
+//3) make sure multiple readers are allowed
+//4) make sure when a writer is writing reader_count should be zero
+
+#define NREADTHREADS 8
+#define NWRITETHREADS 3
+#define RWARRAYSIZE 10
+unsigned int rw_test_array[RWARRAYSIZE];
+#define NREADLOOPS 32
+#define NWRITELOOPS 4
+struct rwlock *rwlock_test;
+struct semaphore *donesem;
+
+
+static
+void
+rwt1reader(void *junk1,unsigned long num)
+{
+	(void)junk1;
+
+	unsigned i,j;
+
+	random_yielder(4);
+	for(i=0;i<NREADLOOPS;i++) {
+		kprintf("reader %u: entered\n",(unsigned int)num);
+		rwlock_acquire_read(rwlock_test);
+		kprintf("reader %u ",(unsigned int)num);
+		for(j=0;j<RWARRAYSIZE;j++){
+			kprintf("%u ",rw_test_array[j]);
+		}
+		kprintf("\n");
+		rwlock_release_read(rwlock_test);
+		random_yielder(4);
+	}
+	V(donesem);
+}
+
+static
+void
+rwt1writer (void *junk1, unsigned long num)
+{
+	(void)junk1;
+	unsigned i,j;
+	random_yielder(8);
+	for(i=0;i<NWRITELOOPS;i++) {
+		kprintf("writer %u: entered\n",(unsigned int)num);
+		rwlock_acquire_write(rwlock_test);
+		for(j=0;j<RWARRAYSIZE;j++) {
+			rw_test_array[j]=num*10+j;
+		}
+		rwlock_release_write(rwlock_test);
+		random_yielder(8);
+	}
+	V(donesem);
+}
+
 int rwtest(int nargs, char **args) {
 	(void)nargs;
 	(void)args;
 
-	kprintf_n("rwt1 unimplemented\n");
-	success(TEST161_FAIL, SECRET, "rwt1");
+	/*kprintf_n("rwt1 unimplemented\n");*/
+	/*test code start*/
+	int i,result;
+	
+	donesem = sem_create("rwtdonesem",0);
+	KASSERT(donesem!=NULL);
+	rwlock_test = rwlock_create("rwtest1");
+	KASSERT(rwlock_test!=NULL);
+
+	for(i=0;i<RWARRAYSIZE;i++){
+		rw_test_array[i]=i+40;	
+	}
+	//create reader threads
+	for(i=0;i<NREADTHREADS;i++) {
+		result = thread_fork("rwt1reader",NULL,rwt1reader,NULL,(long unsigned)i);
+	if (result) {
+			panic("rwt1: thread_fork failed: %s\n", strerror(result));
+		}
+	}
+
+	//create writer threads
+	for(i=0;i<NWRITETHREADS;i++) {
+		result = thread_fork("rwt1writer",NULL,rwt1writer,NULL,(long unsigned)i);
+	if (result) {
+			panic("rwt1: thread_fork failed: %s\n", strerror(result));
+		}
+	}
+	for(i=0;i<(NREADTHREADS+NWRITETHREADS);i++) {
+		P(donesem);
+	}
+
+	sem_destroy(donesem);
+	rwlock_destroy(rwlock_test);
+	kprintf("----------TEST DONE-------------\n");
+
+	/*test code end*/
+	success(TEST161_SUCCESS, SECRET, "rwt1");
 
 	return 0;
 }
