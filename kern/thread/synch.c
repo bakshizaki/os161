a498e41e9de391f0a517f7ca46606b2ba79968e9
Zaki Bakshi <bakshizaki@gmail.com>
2017-02-09 17:51:47 -0500

    minor changes, nothing important

--- kern/thread/synch.c
+++ kern/thread/synch.c
@@ -219,7 +219,7 @@ lock_do_i_hold(struct lock *lock)
 {
 	// Write this
 
-	(void)lock;  // suppress warning until code gets written
+	/*(void)lock;  // suppress warning until code gets written*/
 
 	/*return true; // dummy until code gets written*/
 	return(lock->lk_thread == curthread);
